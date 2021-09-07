6b5fa80a033994e5b2117f66bda1aac1fad0d9f8
Jamie De Vivo <accounts@jamiedevivo.com>
2021-09-07 19:41:54 +0100

    Twilwind installed

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,39 @@
 <script lang="typescript">
 	import { Router } from "@roxi/routify";
 	import { routes } from "../.routify/routes";
+	import TailwindCss from "./tailwindCSS.svelte";
+
+	import { Haptics, ImpactStyle } from '@capacitor/haptics';
+
+const hapticsImpactMedium = async () => {
+  await Haptics.impact({ style: ImpactStyle.Medium });
+};
+
+const hapticsImpactLight = async () => {
+  await Haptics.impact({ style: ImpactStyle.Light });
+};
+
+const hapticsVibrate = async () => {
+  await Haptics.vibrate();
+};
+
+const hapticsSelectionStart = async () => {
+  await Haptics.selectionStart();
+};
+
+const hapticsSelectionChanged = async () => {
+  await Haptics.selectionChanged();
+};
+
+const hapticsSelectionEnd = async () => {
+  await Haptics.selectionEnd();
+};
 </script>
 
-<Router {routes} />
+<TailwindCss />
 
+<Router {routes} />
+<p>Hey</p>
 <style @global>
 
 </style>
